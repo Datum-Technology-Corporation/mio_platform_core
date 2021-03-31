a71f54dae1d56d73af4b7c5a452f4b3d2efe19a4
David Poulin <dpoulin@datumtc.ca>
2021-03-30 20:52:50 -0400

    First version of cli.py

---
+++ cli.py
@@ -0,0 +1,18 @@
+"""Design Verification \'Makefile\'.
+
+Usage:
+  dvm.py all  <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-d | --debug]  [-w | --waves] [-q | --noclean]
+  dvm.py cmp  <target>
+  dvm.py elab <target>  [-d | --debug]
+  dvm.py cpel <target>
+  dvm.py sim  <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-w | --waves]
+  dvm.py clean
+  dvm.py update
+  dvm.py (-h | --help)
+  dvm.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
