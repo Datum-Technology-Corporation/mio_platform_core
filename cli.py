cc073bed39e9483da6e8327d99c22fc856dd8a94
David Poulin <dpoulin@datumtc.ca>
2021-03-31 23:54:33 -0400

    updating cli.py

--- cli.py
+++ cli.py
@@ -1,16 +1,62 @@
-"""Design Verification \'Makefile\'.
+"""'Moore.io Core CLI'.
 
 Usage:
-  dvm.py all  <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-d | --debug]  [-w | --waves] [-q | --noclean]
-  dvm.py cmp  <target>
-  dvm.py elab <target>  [-d | --debug]
-  dvm.py cpel <target>
-  dvm.py sim  <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-w | --waves]
-  dvm.py clean
-  dvm.py update
-  dvm.py (-h | --help)
-  dvm.py --version
+  # Digital Logic Simulation
+  mio clean [<target>] [-a | --all] [-w | --wipe_results]
+  mio lib    <target>  [-nr | --non_recursive]
+  mio sim    <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-w | --waves]
+  mio cmp    <target>
+  mio elab   <target>  [-d | --debug]
+  mio cpel   <target>  [-d | --debug]
+  mio oss    <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-d | --debug]  [-w | --waves]  [-q | --noclean]
+  mio rrg    <test_suite>  <regression_name>
+  
+  # Generators & Refactoring & Documentation Generator -- Cool mio exclusives!
+  mio new      [<generator_name>]
+  mio refactor <target>  (disconnect|connect|rename|delete)
+  mio docgen [target_ip]  [-a | --all]
+  
+  # Misc. EDA
+  mio lint # tbd
+  mio synt # tbd
+  
+  mio doctor
+  mio (-h | --help)
+  mio --version
 
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
+
+
+"""'Moore.io IP Manager CLI'.
+
+Usage:
+  mpm init
+  mpm config
+  
+  mpm search
+  mpm install
+  mpm uninstall
+  mpm list
+  mpm prune
+  mpm outdated
+  mpm update
+  
+  mpm login
+  mpm logout
+  
+  mpm publish
+  mpm unpublish
+  mpm deprecate
+  mpm shrinkwrap
+  
+  mio doctor
+  mio (-h | --help)
+  mio --version
+  
 Options:
   -h --help     Show this screen.
   --version     Show version.
